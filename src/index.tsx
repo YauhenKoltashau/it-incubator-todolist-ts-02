6b73debd501f6e7f4d10e9299ba2c2f0aed30f36
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-12-02 13:22:38 +0300

    refactored 15.3

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,14 @@ import * as serviceWorker from './serviceWorker';
 import App from "./app/App";
 import {Provider} from "react-redux";
 import {store} from "./app/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
         <App/>
         {/*<AppWithReducers/>*/}
-        </BrowserRouter>
+        </HashRouter>
     </Provider>
     , document.getElementById('root'));
 
